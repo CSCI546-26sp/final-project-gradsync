b961d0e89db1a722ba2dbf17d75c5ce43bad5a85
Bhaskar Mahajan <bhaskarmahajan98@gmail.com>
2026-03-29 20:16:40 -0700

    export module

--- packages/pipeline/src/pipeline/__init__.py
+++ packages/pipeline/src/pipeline/__init__.py
@@ -1,3 +1,7 @@
-def hello() -> str:
-    return "Hello from pipeline!"
+from .core import DistributedPipeline
+from .runner import HeadNodeRunner, TailNodeRunner
+from .utils import detect_device
+
+__all__ = ["DistributedPipeline"]
+
 
